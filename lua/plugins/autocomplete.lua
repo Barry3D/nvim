e98ddcf22aef418653b3fd085f8674eaade12368
Frederik Günther <frederik.guenther@iserv.eu>
2025-04-02 09:22:49 +0200

    Adjusted autocomplete init and reduced eagerness

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,9 +1,19 @@
 return {
-    'ms-jpq/coq_nvim',
-    branch = 'coq',
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+        {
+            'ms-jpq/coq_nvim',
+            branch = 'coq',
+        }
+    },
     init = function()
         vim.g.coq_settings = {
             auto_start = true,
+            completion = {
+                skip_after = {"{", "}", "[", "]", ";", " "},
+            }
         }
     end,
 }
+
