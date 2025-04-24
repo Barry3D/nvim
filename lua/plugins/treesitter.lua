4f7cc31961bd32e46767e8b468817db37571e6d6
Frederik Günther <frederik.guenther@iserv.eu>
2025-04-24 12:46:02 +0200

    Added treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,45 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })()
+        end,
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },            
-            sync_install = true,
-        })
-    end,
+            configs.setup({
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },            
+                sync_install = true,
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        event = "VeryLazy",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["aa"] = "@parameter.outer",
+                            ["ia"] = "@parameter.inner",
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                        },
+                        selection_modes = {
+                            ["@function.outer"] = "V",
+                            ["@class.outer"] = "V",
+                        },
+                    },
+                },
+            })
+        end,
+    }
 }
